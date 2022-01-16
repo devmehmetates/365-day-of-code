c70b8e0e0e2bf977d60f13d6eb4b51f78b8e161f
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-16 13:01:11 +0300

    Day16(Project1-1)

---
+++ Project/Day16-18/Day16-18/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Day16-18
+//
+//  Created by Mehmet Ateş on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+            
+        }
+        print(pictures)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
