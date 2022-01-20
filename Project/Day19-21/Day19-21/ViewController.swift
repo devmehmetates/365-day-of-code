f0582e27d865dcd81407384c588c33f02a8037d3
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-20 17:11:21 +0300

    Day 20 Completed

--- Project/Day19-21/Day19-21/ViewController.swift
+++ Project/Day19-21/Day19-21/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +31,35 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
     }
     
-    func askQuestion(){
+    func askQuestion(action:UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased() 
     }
 
-
+    @IBAction func buttonTabbed(_ sender: UIButton) {
+        var title : String
+        
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac,animated: true)
+        
+        
+    }
+    
 }
 
