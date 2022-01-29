c9ee1cf9271ac522891348d29bd7696719dd4eda
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-29 15:38:34 +0300

    Day29 completed

--- Project/Day27-29/Day27-29/ViewController.swift
+++ Project/Day27-29/Day27-29/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(resetGame))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         if let startUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
@@ -28,6 +30,10 @@ class ViewController: UITableViewController {
         
         startGame()
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func resetGame(){
+        startGame()
     }
     
     func startGame(){
@@ -59,37 +65,34 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    func showErrorMessage(title : String, message : String){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac,animated: true)
+    }
+    
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
-        
-        let errorTitle : String
-        let errorMessage : String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer){
                 if isReal(word: lowerAnswer){
                     usedWords.insert(answer, at: 0)
-                    
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     return
                 }else{
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(title: "Word not recognized", message: "You can't just make them up, you know!")
                 }
             }else{
-                errorTitle = "Already Used"
-                errorMessage = "Be more original"
+                showErrorMessage(title: "Already Used", message: "Be more original")
             }
         }else{
             guard let title = title else{return}
-            errorTitle = "word not possible"
-            errorMessage = "You can't spell that word from \(title.lowercased())."
+            showErrorMessage(title: "word not possible", message: "You can't spell that word from \(title.lowercased()).")
         }
         
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac,animated: true)
+        
         
     }
     
@@ -107,12 +110,30 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal (word : String) -> Bool{
-        return !usedWords.contains(word)
+        if word.lowercased() == title?.lowercased(){
+            return false
+        }
+        
+        for usedWord in usedWords {
+            if usedWord.lowercased() == word.lowercased(){
+                return false
+            }else{
+                return true
+            }
+        }
+        
+        return true
+        
+        
     }
     
     func isReal(word : String) -> Bool{
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
+        
+        if range.length < 3{
+            return false
+        }
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
         return misspelledRange.location == NSNotFound
