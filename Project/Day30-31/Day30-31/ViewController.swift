c8e4e771df18cf88a3532adb06f48d8805796c2a
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-30 17:53:27 +0300

    Project6 Setup

---
+++ Project/Day30-31/Day30-31/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by TwoStraws on 13/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet var button1: UIButton!
+	@IBOutlet var button2: UIButton!
+	@IBOutlet var button3: UIButton!
+
+	var countries = [String]()
+	var correctAnswer = 0
+	var score = 0
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		button1.layer.borderWidth = 1
+		button2.layer.borderWidth = 1
+		button3.layer.borderWidth = 1
+
+		button1.layer.borderColor = UIColor.lightGray.cgColor
+		button2.layer.borderColor = UIColor.lightGray.cgColor
+		button3.layer.borderColor = UIColor.lightGray.cgColor
+
+		countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+		askQuestion()
+	}
+
+	func askQuestion(action: UIAlertAction! = nil) {
+		countries.shuffle()
+
+		button1.setImage(UIImage(named: countries[0]), for: .normal)
+		button2.setImage(UIImage(named: countries[1]), for: .normal)
+		button3.setImage(UIImage(named: countries[2]), for: .normal)
+
+        correctAnswer = Int.random(in: 0...2)
+		title = countries[correctAnswer].uppercased()
+	}
+
+	@IBAction func buttonTapped(_ sender: UIButton) {
+		var title: String
+
+		if sender.tag == correctAnswer {
+			title = "Correct"
+			score += 1
+		} else {
+			title = "Wrong"
+			score -= 1
+		}
+
+		let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+		present(ac, animated: true)
+	}
+
+}
