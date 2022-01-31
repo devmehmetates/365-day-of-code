f5b37f635a7845176d02a43ade6883ff50fb7d53
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-31 16:10:39 +0300

    Day31 completed without challenge

--- Project/Day30-31-2/Day30-31-2/ViewController.swift
+++ Project/Day30-31-2/Day30-31-2/ViewController.swift
@@ -49,13 +49,30 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1" : label1 , "label2" : label2, "label3" : label3, "label4" : label4 , "label5" : label5]
+//        let viewsDictionary = ["label1" : label1 , "label2" : label2, "label3" : label3, "label4" : label4 , "label5" : label5]
+//
+//        for label in viewsDictionary.keys{
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight" : 88]
+//
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         
-        for label in viewsDictionary.keys{
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        var previous : UILabel?
+        
+        for label in [label1, label2, label3, label4, label5]{
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor,constant: 10).isActive = true
+            } else{
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 0).isActive = true
+            }
+            previous = label
         }
-        
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
         
         
         // Do any additional setup after loading the view.
