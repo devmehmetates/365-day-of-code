9bb3c3c623028014c410e1ee8d6b57e931104725
Mehmet ateş <Mehmet1095@icloud.com>
2022-02-08 20:07:12 +0300

    Day39 completed

---
+++ Project/Day39-41/Day39-41/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by TwoStraws on 15/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+}
---
+++ Project/Day39-41/Day39-41/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by TwoStraws on 15/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+	var petitions = [Petition]()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+	}
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        }else{
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+    
+    @objc func fetchJSON(){
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return petitions.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+		let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let vc = DetailViewController()
+		vc.detailItem = petitions[indexPath.row]
+		navigationController?.pushViewController(vc, animated: true)
+	}
+
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+}
+
