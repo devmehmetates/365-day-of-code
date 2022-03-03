3599104bc40d8592b56b17e215fcaa4e3a5c8956
Mehmet ateş <Mehmet1095@icloud.com>
2022-03-03 18:41:16 +0300

    Day62 set-up

---
+++ Project/Day62-63/Day62-63/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Day62-63
+//
+//  Created by Mehmet Ateş on 3.03.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
