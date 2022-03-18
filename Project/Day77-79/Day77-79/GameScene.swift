e0db47e760b42ef65da1baf352fde668d0eae42f
Mehmet ateş <Mehmet1095@icloud.com>
2022-03-18 17:12:09 +0300

    Day77 Setup

---
+++ Project/Day77-79/Day77-79/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Day77-79
+//
+//  Created by Mehmet Ateş on 18.03.2022.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+}
