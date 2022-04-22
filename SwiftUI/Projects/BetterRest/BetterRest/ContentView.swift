b4388a0b8d350fa3dc6a6a2b786dbb231da68ad3
Mehmet ateş <Mehmet1095@icloud.com>
2022-04-22 16:18:13 +0300

    Challenges completed

--- SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date.now
     }
     
-    
+    @State private var sleepTime = Date.now
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
@@ -28,35 +28,43 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             Form{
-                VStack(alignment:.leading, spacing:0){
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                Text("Your ideal bedtime is \( sleepTime.formatted(date: .omitted, time: .shortened)) hour.")
+                
+                Section{
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
                 }
                 
-                VStack(alignment:.leading, spacing:0){
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
+                Section{
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
                 }
                 
-                VStack(alignment:.leading, spacing:0){
+                Section{
+                    Picker("How many cups ?", selection: $coffeeAmount) {
+                        ForEach(1..<21) { rangeAmount in
+                            Button(String(rangeAmount)){ coffeeAmount = rangeAmount }
+                                .foregroundColor(.primary)
+                        }
+                    }
+                } header: {
                     Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 }
                 
             }.navigationTitle("BetterRest")
-                .toolbar {
-                    Button("Calculate", action: calculateBedtime)
-                }.alert(alertTitle, isPresented: $showingAlert) {
+                .alert(alertTitle, isPresented: $showingAlert) {
                     Button("OK") { }
                 } message: {
                     Text(alertMessage)
+                }.onChange(of: self.coffeeAmount) { _ in
+                    self.calculateBedtime()
+                }.onChange(of: self.sleepAmount) { _ in
+                    self.calculateBedtime()
+                }.onChange(of: self.wakeUp) { _ in
+                    self.calculateBedtime()
                 }
         }
     }
@@ -72,9 +80,9 @@ struct ContentView: View {
             
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
-            let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            let calculatedSleepTime = wakeUp - prediction.actualSleep
+            self.sleepTime = calculatedSleepTime
+          
         }catch{
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
