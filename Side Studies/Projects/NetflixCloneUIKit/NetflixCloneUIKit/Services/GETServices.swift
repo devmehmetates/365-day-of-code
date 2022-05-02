d85f30d01f6ec96a58fccfb4128db5b400e06eb1
Mehmet ateş <Mehmet1095@icloud.com>
2022-05-02 17:43:24 +0300

    Delete app

--- Side Studies/Projects/NetflixCloneUIKit/NetflixCloneUIKit/Services/GETServices.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GETServices.swift
-//  NetflixCloneUIKit
-//
-//  Created by Mehmet Ateş on 2.05.2022.
-//
-
-import Foundation
--- Side Studies/Projects/NetflixCloneUIKit/Pods/Target Support Files/Pods-NetflixCloneUIKit/Pods-NetflixCloneUIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetflixCloneUIKit : NSObject
-@end
-@implementation PodsDummy_Pods_NetflixCloneUIKit
-@end
