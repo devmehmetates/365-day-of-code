7c65baff26d61260abc8ce3fd0783fb36a1f233e
Mehmet ateş <Mehmet1095@icloud.com>
2022-05-05 23:11:01 +0300

    New Side Projects was created

---
+++ Side Studies/Projects/TrendyolSwiftUI/TrendyolSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrendyolSwiftUI
+//
+//  Created by Mehmet Ateş on 5.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Side Studies/Projects/TrendyolSwiftUI/TrendyolSwiftUI/TrendyolSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendyolSwiftUIApp.swift
+//  TrendyolSwiftUI
+//
+//  Created by Mehmet Ateş on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrendyolSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
