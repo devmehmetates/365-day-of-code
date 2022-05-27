b392c2bc5c90e52443d48265101a05319cc5b8e6
Mehmet ateş <Mehmet1095@icloud.com>
2022-05-27 11:58:31 +0300

    Core image fun

--- SwiftUI/Projects/Instafilter/Instafilter/InstafilterApp.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/InstafilterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InstafilterApp: App {
     var body: some Scene {
         WindowGroup {
-            ShowingMultipleOptions()
+            CoreImageTutorial()
         }
     }
 }
--- SwiftUI/Projects/Instafilter/Instafilter/Tutorial/CoreImageTutorial.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/Tutorial/CoreImageTutorial.swift
@@ -14,12 +14,16 @@ struct CoreImageTutorial: View {
     @State private var image: Image?
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        NavigationView{
+            VStack {
+                image?
+                    .resizable()
+                    .scaledToFill()
+            }
+            .onAppear(perform: loadImage)
+            .navigationTitle("Guess the Picture")
         }
-        .onAppear(perform: loadImage)
+       
     }
     
     func loadImage() {
@@ -28,26 +32,26 @@ struct CoreImageTutorial: View {
         
         let context = CIContext()
         
-//        let currentFilter = CIFilter.pixellate()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.scale = 100
+        let currentFilter = CIFilter.pixellate()
+        currentFilter.inputImage = beginImage
+        currentFilter.scale = 30
         
 //        let currentFilter = CIFilter.crystallize()
 //        currentFilter.inputImage = beginImage
 //        currentFilter.radius = 200
         
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-        
-        let amount = 1.0
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 100, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+//        let currentFilter = CIFilter.twirlDistortion()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+//
+//        let amount = 1.0
+//
+//        let inputKeys = currentFilter.inputKeys
+//
+//        if inputKeys.contains(kCIInputIntensityKey) {
+//            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+//        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 100, forKey: kCIInputRadiusKey) }
+//        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
         
