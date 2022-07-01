c3c32f7de77ddbb87d1d9483aad4b8afdf56345c
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-01 20:18:32 +0300

    Day183 completed

---
+++ SwiftUI/Projects/SnowSeeker/SnowSeeker/Facility.swift
@@ -0,0 +1,48 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by Mehmet Ateş on 1.07.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct Facility: Identifiable {
+    let id = UUID()
+    var name: String
+
+    private let icons = [
+        "Accommodation": "house",
+        "Beginners": "1.circle",
+        "Cross-country": "map",
+        "Eco-friendly": "leaf.arrow.circlepath",
+        "Family": "person.3"
+    ]
+    
+    private let descriptions = [
+        "Accommodation": "This resort has popular on-site accommodation.",
+        "Beginners": "This resort has lots of ski schools.",
+        "Cross-country": "This resort has many cross-country ski routes.",
+        "Eco-friendly": "This resort has won an award for environmental friendliness.",
+        "Family": "This resort is popular with families."
+    ]
+    
+    var description: String {
+        if let message = descriptions[name] {
+            return message
+        } else {
+            fatalError("Unknown facility type: \(name)")
+        }
+    }
+
+    var icon: some View {
+        if let iconName = icons[name] {
+            return Image(systemName: iconName)
+                .accessibilityLabel(name)
+                .foregroundColor(.secondary)
+        } else {
+            fatalError("Unknown facility type: \(name)")
+        }
+    }
+}
---
+++ SwiftUI/Projects/SnowSeeker/SnowSeeker/Favorites.swift
@@ -0,0 +1,46 @@
+//
+//  Favorites.swift
+//  SnowSeeker
+//
+//  Created by Mehmet Ateş on 1.07.2022.
+//
+
+import Foundation
+
+class Favorites: ObservableObject {
+    // the actual resorts the user has favorited
+    private var resorts: Set<String>
+
+    // the key we're using to read/write in UserDefaults
+    private let saveKey = "Favorites"
+
+    init() {
+        // load our saved data
+
+        // still here? Use an empty array
+        resorts = []
+    }
+
+    // returns true if our set contains this resort
+    func contains(_ resort: Resort) -> Bool {
+        resorts.contains(resort.id)
+    }
+
+    // adds the resort to our set, updates all views, and saves the change
+    func add(_ resort: Resort) {
+        objectWillChange.send()
+        resorts.insert(resort.id)
+        save()
+    }
+
+    // removes the resort from our set, updates all views, and saves the change
+    func remove(_ resort: Resort) {
+        objectWillChange.send()
+        resorts.remove(resort.id)
+        save()
+    }
+
+    func save() {
+        // write out our data
+    }
+}
